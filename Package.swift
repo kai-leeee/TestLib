9488df8214be2915df644f8aa496f86ab1b7e242
Kai Lee <kai.lee@sendbird.com>
2025-09-04 15:36:57 +0900

    ddd

--- Package.swift
+++ Package.swift
@@ -16,6 +16,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "TestLib"),
+        .target(
+            name: "TestLib23"),
         .testTarget(
             name: "TestLibTests",
             dependencies: ["TestLib"]
